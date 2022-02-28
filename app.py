25f1f907529d558dfd63a607ba47f102f6c43fea
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-03-01 00:00:49 +0300

    everything works, submitted

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 import telebot
 from config import codes_human
 from telebot import types
+from datetime import date
 from extensions import Converter, APIException
 
-
-#TODO база данный по субтитрам
 
 TOKEN = "5156928592:AAHs5egCtuMoEZl7xxyg3yRI0iQtSU4XfQo"
 
 bot = telebot.TeleBot(TOKEN)
 
-markup = types.ReplyKeyboardMarkup(row_width=5, one_time_keyboard=False, resize_keyboard=False)
+markup_codes = types.ReplyKeyboardMarkup(row_width=10, one_time_keyboard=True, resize_keyboard=False)
 buttons = []
-for code, name in codes_human.items():
+for code in codes_human.keys():
     buttons.append(types.KeyboardButton(code.upper()))
+markup_codes.add(*buttons)
 
-markup.add(*buttons)
+markup_numbers = types.ReplyKeyboardMarkup(row_width=4, one_time_keyboard=True, resize_keyboard=False)
+numbers = ['1', '5', '10', '20', '50', '100', '200', '500', '1000']
+markup_numbers.add(*numbers)
 
+markup_commands = types.InlineKeyboardMarkup()
+command1 = types.InlineKeyboardButton("Buttons mode", callback_data='switch_to_buttons')
+command2 = types.InlineKeyboardButton("List of currencies", callback_data='show_available_currencies')
+markup_commands.add(command1, command2)
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'switch_to_buttons')
+def on_error_buttons(callback_query: types.CallbackQuery):
+    bot.answer_callback_query(callback_query.id)
+    bot.send_message(callback_query.from_user.id, '/buttons')
+
+@bot.callback_query_handler(func=lambda c: c.data == 'show_available_currencies')
+def on_error_buttons(callback_query: types.CallbackQuery):
+    #bot.answer_callback_query(callback_query.id)
+    bot.send_message(callback_query.from_user.id, '/values')
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message) -> None:
@@ -36,29 +53,24 @@ def handle_values(message: telebot.types.Message) -> None:
     text = 'Available currencies:\n'
     for code, name in codes_human.items():
         text = ''.join((text, code+': ', name[1]+'\n'))
-    bot.send_message(message.chat.id, text)
+    bot.send_message(message.chat.id, text, reply_markup=markup_commands)
     
 @bot.message_handler(commands=['convert', 'buttons'])
 def handle_convert(message):
     text = "Choose the (base) currency to convert from:"
-    bot.send_message(message.chat.id, text, reply_markup=markup)
+    bot.send_message(message.chat.id, text, reply_markup=markup_codes)
     bot.register_next_step_handler(message, source_handler)
     
 def source_handler(message):
     source = message.text.strip()
     text = "Now choose the (target) currency to convert into:"
-    bot.send_message(message.chat.id, text, reply_markup=markup)
-    try: #TODO
-        assert(message.text == source)
-    except APIException as e:
-        bot.send_message(message.chat.id, f"You're trying to convert {source} into {source}.")
-    else:
-        bot.register_next_step_handler(message, target_handler, source)
+    bot.send_message(message.chat.id, text, reply_markup=markup_codes)
+    bot.register_next_step_handler(message, target_handler, source)
     
 def target_handler(message, source):
     target = message.text.strip()
-    text = "Now type the amount of your base currency:"
-    bot.send_message(message.chat.id, text)
+    text = "Now choose the amount of your base currency or press a button:"
+    bot.send_message(message.chat.id, text, reply_markup=markup_numbers)
     bot.register_next_step_handler(message, conv_handler, source, target)
     
 def conv_handler(message, source, target):
@@ -66,25 +78,33 @@ def conv_handler(message, source, target):
     try:
         result_amount = Converter.get_price(source, target, amount)
     except APIException as e:
-        bot.send_message(message.chat.id, f"Conversion error: \n{e}\nTry again.")
+        bot.send_message(message.chat.id, f"Conversion error: \n{e}\nTry again.", reply_markup=markup_commands)
     else:
-        text = f"{amount} {codes_human[source][0]} = {result_amount} {codes_human[target][0]}."
-        bot.send_message(message.chat.id, text)
+        text = f'Today, {date.today().strftime("%B %d, %Y")}, \n'
+        text += f'\n<b>{amount} {codes_human[source][0]} = {result_amount} {codes_human[target][0]}</b>.\n'
+        text += f'\nData received from www.cbr.ru, Central Bank of Russia site: \
+            <a>https://www.cbr.ru/currency_base/daily/</a>'
+        bot.send_message(message.chat.id, text, parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
 def handle_text_conversion(message):
-    msg = message.text.strip().split()
-    amount = msg[0]
-    source = msg[1]
-    target = msg[2]
     
     try:
+        msg = message.text.strip().upper().split()
+        amount = msg[0]
+        source = msg[1]
+        target = msg[2]
         result_amount = Converter.get_price(source, target, amount)
+    except IndexError:
+        bot.send_message(message.chat.id, f"Conversion error.\nTry again.", reply_markup=markup_commands)
     except APIException as e:
-        bot.send_message(message.chat.id, f"Conversion error: \n{e}\nTry again.")
+        bot.send_message(message.chat.id, f"Conversion error: \n{e}\nTry again.", reply_markup=markup_commands)
     else:
-        text = f"{amount} {codes_human[source][0]} = {result_amount} {codes_human[target][0]}."
-        bot.send_message(message.chat.id, text)
+        text = f'Today, {date.today().strftime("%B %d, %Y")}, \n'
+        text += f'\n<b>{amount} {codes_human[source][0]} = {result_amount} {codes_human[target][0]}.</b>\n'
+        text += f'\nData received from www.cbr.ru, Central Bank of Russia site: \
+            <a>https://www.cbr.ru/currency_base/daily/</a>'
+        bot.send_message(message.chat.id, text, parse_mode='html')
 
 
-bot.polling(none_stop=True)
+bot.polling(non_stop=True)
